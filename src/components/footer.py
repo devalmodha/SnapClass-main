27d2d2c323d71492557dc53a9e4e20be8d84d28c
deval modha <devalmodha555@gmail.com>
2026-06-07 17:07:45 +0530

    fix footer text

--- src/components/footer.py
+++ src/components/footer.py
@@ -5,7 +5,7 @@ def footer_home():
     logo_url = "https://i.ibb.co/YTYGn5qV/logo.png"
     st.markdown(f"""
                 <div style = "margin-top:2rem; display:flex; gap : 6px;justify-content :center; item-align : center ">
-                <p style="color : white; font-weight : bold">Created Deval Modha</p>
+                <p style="color : white; font-weight : bold">Created by Deval Modha</p>
                 </div>    
             """,unsafe_allow_html=True)
 
