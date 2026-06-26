129aa95f80778fa0816d0c53f122b1da8859f774
deval modha <devalmodha555@gmail.com>
2026-06-26 18:00:46 +0530

    chanes done

--- src/database/db.py
+++ src/database/db.py
@@ -27,18 +27,9 @@ def teacher_login(username, password):
 
 
 def get_all_students():
-    # response = supabase.table("students").select("*").execute()
-    # return response.data
-    max_retries = 3
-    for attempt in range(max_retries):
-        try:
-            response = supabase.table("students").select("*").execute()
-            return response.data
-        except Exception as e:
-            if attempt < max_retries - 1:
-                time.sleep(2)
-                continue
-            raise e
+    response = supabase.table("students").select("*").execute()
+    return response.data
+    
 
 def create_student(new_name,face_embedding = None , voice_embedding = None):
     data = {'name': new_name,'face_embedding':face_embedding,'voice_embedding': voice_embedding}
